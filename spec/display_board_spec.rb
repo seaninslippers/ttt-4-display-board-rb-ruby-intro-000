15c304f789fbd00d6c33b10d2e0ec85f136bc771
Sean Xiong <seanxionghe@gmail.com>
2020-04-06 20:56:37 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -186,7 +186,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | O ")
-      end
+    end
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
